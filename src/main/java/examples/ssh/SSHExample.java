f2a54b5b68ca388fcdc689041bc2025198b1dae7
shikhrr <shikhrr@60bb307e-432b-11de-998b-8fedbbe774c7>
2009-07-02 23:09:24 +0000

    example update

--- src/main/java/examples/ssh/SSHExample.java
+++ src/main/java/examples/ssh/SSHExample.java
@@ -1,7 +1,6 @@
 package examples.ssh;
 
 import org.apache.commons.net.ssh.SSHClient;
-import org.apache.commons.net.ssh.userauth.UserAuthService.PasswordFinder;
 import org.apache.log4j.BasicConfigurator;
 
 public class SSHExample
@@ -13,17 +12,17 @@ public class SSHExample
     
     public static void main(String[] args) throws Exception
     {
-        
         SSHClient client = new SSHClient();
         client.loadKnownHosts();
-        client.connect("localhost");
-        client.getAuthBuilder().withUsername("bleh").authPassword(new PasswordFinder()
-        {
-            public char[] getPassword()
-            {
-                return "abcdef".toCharArray();
-            }
-        }).build().authenticate();
+        client.connect("127.0.0.1");
+        
+        client.getAuthBuilder().withPassword("abcdef").authPublickey("/home/shx/.ssh/id_dsa",
+                "/home/shx/.ssh/clamv").build().authenticate();
+        
+        // client.getAuthBuilder().withUsername("bleh").authPassword("abcdef").build().authenticate();
+        
         client.disconnect();
+        
     }
+    
 }
