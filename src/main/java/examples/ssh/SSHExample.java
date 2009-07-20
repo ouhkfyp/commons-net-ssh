aeabc6b1999da97e255386ac210050b71a52e967
shikhrr <shikhrr@60bb307e-432b-11de-998b-8fedbbe774c7>
2009-07-20 12:01:19 +0000

    AES CTR (128, 192 & 256 bit) ciphers; example update

--- src/main/java/examples/ssh/SSHExample.java
+++ src/main/java/examples/ssh/SSHExample.java
@@ -6,12 +6,14 @@ import org.apache.commons.net.ssh.SSHClient;
 import org.apache.commons.net.ssh.connection.Session;
 import org.apache.commons.net.ssh.connection.Session.Command;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.PatternLayout;
 
 public class SSHExample
 {
     
     static {
-        BasicConfigurator.configure();
+        BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("[%t] %p %c{2} %m%n")));
     }
     
     public static void main(String[] args) throws Exception
@@ -22,30 +24,33 @@ public class SSHExample
         client.initUserKnownHosts();
         //        client.addHostKeyVerifier("e6:d4:18:e6:c8:2d:29:a3:83:ae:aa:d5:fc:f2:49:47");
         
-        //        client.connect("schmizz.net");
         long startTime = System.nanoTime();
         
-        client.connect("localhost");
+        //        client.connect("localhost");
+        client.connect("schmizz.net");
+        Session session = null;
         try {
             
-            //            client.authPassword("bobo", "abcdef".toCharArray());
+            //            client.authPassword("bobo", "acdef".toCharArray());
             client.authPublickey("shikhar", client.loadKeyFile("/home/shikhar/.ssh/id_rsa"));
-            
-            Session session = client.startSession();
-            Command cmd = session.exec("man ls");
+            session = client.startSession();
+            Command cmd = session.exec("man ssh_config");
             
             StringBuilder sb = new StringBuilder();
-            InputStreamReader reader = new InputStreamReader(cmd.getIn());
+            InputStreamReader reader = new InputStreamReader(cmd.getInputStream());
             int read;
             char[] cbuf = new char[256];
             while ((read = reader.read(cbuf)) != -1)
                 sb.append(cbuf, 0, read);
             
+            //            System.out.print(client.getAuthBanner());
             System.out.print(sb);
             System.out.println("Exit status: " + cmd.getExitStatus());
             
         } finally {
             System.out.println("*** took " + (System.nanoTime() - startTime) / 1000000000.0 + " seconds.");
+            if (session != null)
+                session.close();
             client.disconnect();
         }
         
