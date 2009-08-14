134a8d3f3d856d58ec79b2fd66052d5824f654c3
shikhrr <shikhrr@60bb307e-432b-11de-998b-8fedbbe774c7>
2009-08-14 23:14:31 +0000

    updat opensshkeyfiletest

--- src/test/java/org/apache/commons/net/ssh/keyprovider/OpenSSHKeyFileTest.java
+++ src/test/java/org/apache/commons/net/ssh/keyprovider/OpenSSHKeyFileTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
 
+import org.apache.commons.net.ssh.util.KeyUtil;
 import org.apache.commons.net.ssh.util.PasswordFinder;
 import org.apache.commons.net.ssh.util.Constants.KeyType;
 import org.apache.sshd.common.util.SecurityUtils;
@@ -18,7 +19,9 @@ public class OpenSSHKeyFileTest
     
     private boolean readyToProvide;
     
-    PasswordFinder pwdf = new PasswordFinder()
+    private final FileKeyProvider dsa = new OpenSSHKeyFile();
+    
+    private final PasswordFinder pwdf = new PasswordFinder()
         {
             public char[] reqPassword(Resource resource)
             {
@@ -35,8 +38,6 @@ public class OpenSSHKeyFileTest
             
         };
     
-    private final FileKeyProvider dsa = new OpenSSHKeyFile();
-    
     @Before
     public void setUp() throws UnsupportedEncodingException, GeneralSecurityException
     {
@@ -47,11 +48,24 @@ public class OpenSSHKeyFileTest
     }
     
     @Test
-    public void testKeys() throws IOException
+    public void testKeys() throws IOException, GeneralSecurityException
     {
-        dsa.getPublic(); // TODO - actually compare the key with what is expected
+        final String g =
+                "23b0484f5ad9cba2b3dba7129419fbec7f8c014e22d3b19de4ebbca20d0ebd2e9f5225dabdd48de75f87e3193377fb1072c08433f82f6e6e581a319d4fc7d283cdcd2ae2000fe572c0a800fd47b7590d6a6afe3df54aedd57696c6538029daebf11d9e277edc0c7e905e237d3b9e6a6f674d83da5cc0131ac0be2e55ac69730e";
+        final String p =
+                "92b746cf7c0e9ea35fd9b09b0c3dbdfde453468984698ff168fefef3f0457d29bcf81c88830ac1099223d00745423e44cdef66f4cdc3fad1d95ce2868b3e885c1d518c9fcda597d5c373f05f6f323553f60bd992404183dab41d82ab6d3b3ecf2dfc3c136fa67c4312ec0b7bbac77a634e1eb5dd9a62efd0ddab477d0b49c0b9";
+        final String q = "96a05e07b9e52d6f1137d11d5d270b568b94162f";
+        
+        final String y =
+                "7e845aada202d31004c52ab170cbe62ce9a962b9f4acbc67a57f62eb090a67b3faa53d38050f87b2b66ddf1185472f27842c3e3e58d025f9148a28f49ebdfb6efefee8ee10fe84a2d56535dddb301dfee15538108639e8a0ec7aa237ddb999f35b6a5c6b875052998233374163ad031f974d29c2631394436ae186b418348193";
+        
+        final String x = "8981aebb71c60b5951f0ab3ed1a00b5307742f43";
+        
+        assertEquals(KeyUtil.newDSAPublicKey(y, p, q, g), dsa.getPublic());
+        
         readyToProvide = true;
-        dsa.getPrivate(); // TODO - actually compare the key with what is expected
+        
+        assertEquals(KeyUtil.newDSAPrivateKey(x, p, q, g), dsa.getPrivate());
     }
     
     @Test
--- src/test/java/org/apache/commons/net/ssh/util/KeyUtil.java
+++ src/test/java/org/apache/commons/net/ssh/util/KeyUtil.java
@@ -4,6 +4,8 @@ import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.DSAPrivateKeySpec;
+import java.security.spec.DSAPublicKeySpec;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
 
@@ -11,31 +13,41 @@ public class KeyUtil
 {
     
     /**
+     * Creates a DSA private key.
+     */
+    public static PrivateKey newDSAPrivateKey(String x, String p, String q, String g) throws GeneralSecurityException
+    {
+        return SecurityUtils.getKeyFactory("DSA").generatePrivate(new DSAPrivateKeySpec //
+                                                                  (new BigInteger(x, 16), //
+                                                                   new BigInteger(p, 16), //
+                                                                   new BigInteger(q, 16), // 
+                                                                   new BigInteger(g, 16)));
+    }
+    
+    /**
+     * Creates a DSA public key.
+     */
+    public static PublicKey newDSAPublicKey(String y, String p, String q, String g) throws GeneralSecurityException
+    {
+        return SecurityUtils.getKeyFactory("DSA").generatePublic(new DSAPublicKeySpec //
+                                                                 (new BigInteger(y, 16), //
+                                                                  new BigInteger(p, 16), //
+                                                                  new BigInteger(q, 16), // 
+                                                                  new BigInteger(g, 16)));
+    }
+    
+    /**
      * Creates an RSA private key.
-     * 
-     * @param modulus
-     *            modulus (hex)
-     * @param exponent
-     *            private exponent (hex)
-     * @return the generated private key
-     * @throws GeneralSecurityException
      */
     public static PrivateKey newRSAPrivateKey(String modulus, String exponent) throws GeneralSecurityException
     {
         return SecurityUtils.getKeyFactory("RSA").generatePrivate(new RSAPrivateKeySpec //
-                                                                  (new BigInteger(modulus, 16),
+                                                                  (new BigInteger(modulus, 16), //
                                                                    new BigInteger(exponent, 16)));
     }
     
     /**
      * Creates an RSA public key.
-     * 
-     * @param modulus
-     *            modulus (hex)
-     * @param exponent
-     *            public exponent (hex)
-     * @return the generated public key
-     * @throws GeneralSecurityException
      */
     public static PublicKey newRSAPublicKey(String modulus, String exponent) throws GeneralSecurityException
     {
