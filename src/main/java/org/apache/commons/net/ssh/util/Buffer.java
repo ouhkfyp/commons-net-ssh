961541bcbf9adac651cc9c01d303628349baf6f0
shikhrr <shikhrr@60bb307e-432b-11de-998b-8fedbbe774c7>
2009-12-15 11:21:23 +0000

    license header

--- src/main/java/org/apache/commons/net/ssh/util/Buffer.java
+++ src/main/java/org/apache/commons/net/ssh/util/Buffer.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.commons.net.ssh.util;
 
 import java.io.UnsupportedEncodingException;
@@ -175,7 +193,7 @@ public class Buffer<T extends Buffer<T>>
     
     public byte[] getCompactData()
     {
-        int len = available();
+        final int len = available();
         if (len > 0)
         {
             byte[] b = new byte[len];
